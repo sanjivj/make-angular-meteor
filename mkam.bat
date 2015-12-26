b44a88f96f2c8a6faeb5b8048c4ed523640e9848
sanjivj <sanjivchristopher@gmail.com>
2015-12-26 14:44:49 -0800

    Create mkam.bat
    Batch file for Windows environment.

---
+++ mkam.bat
@@ -0,0 +1,8 @@
+REM This script generates an Angular-Meteor app.
+
+REM First, let us get rid of things that are in the way.
+meteor remove blaze-html-templates
+meteor remove ecmascript
+
+REM We now add angular.
+meteor add angular
