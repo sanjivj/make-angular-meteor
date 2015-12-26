18e7d07c39da58114d14a3d9582a32957d1114be
sanjivj <sanjivchristopher@gmail.com>
2015-12-26 23:34:08 +0000

    Neatened up comments.

--- mkam.bat
+++ mkam.bat
@@ -1,5 +1,4 @@
 REM This script generates an Angular-Meteor app.
-<<<<<<< HEAD
 REM First, create the app.
 meteor create %1
 
@@ -9,12 +8,3 @@ meteor remove ecmascript
 
 REM We now add Angular.
 meteor add angular
-=======
-
-REM First, let us get rid of things that are in the way.
-meteor remove blaze-html-templates
-meteor remove ecmascript
-
-REM We now add angular.
-meteor add angular
->>>>>>> b44a88f96f2c8a6faeb5b8048c4ed523640e9848
