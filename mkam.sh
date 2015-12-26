48936ff42a36291283ea5fffc58e6c2fda9e9270
sanjivj <sanjivchristopher@gmail.com>
2015-12-26 21:09:01 +0000

    Starting a separate repo for mkam, the script to make Angular-Meteor projects.

---
+++ mkam.sh
@@ -0,0 +1,8 @@
+# This script generates an Angular-Meteor app.
+
+# First, let us get rid of things that are in the way.
+meteor remove blaze-html-templates
+meteor remove ecmascript
+
+# We now add angular.
+meteor add angular
