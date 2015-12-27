6109c5cda05be2791fade23127e23eaed0987293
sanjivj <sanjivchristopher@gmail.com>
2015-12-27 16:36:08 +0000

    Put in conditional code in case user forgets to specify an app name.

--- mkam.bat
+++ mkam.bat
@@ -1,6 +1,16 @@
-REM This script generates an Angular-Meteor app.
+REM This batch file generates an Angular-Meteor app.
 REM First, create the app.
-meteor create %1
+REM Type mkam myNewAngularMeteorApp (the name of your app).
+REM If you just type 'mkam', it will create an app with the name 'myApp'.
+REM
+
+if [ "%1" != "" ]; then
+    appName=%1
+else
+    appName='myApp'
+fi
+
+meteor create appName
 
 REM Next, let us get rid of things that are in the way.
 meteor remove blaze-html-templates
--- mkam.sh
+++ mkam.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # This script generates an Angular-Meteor app.
 # First, create the app.
-meteor create $1
+# Type mkam.sh myNewAngularMeteorApp (the name of your app).
+# If you just type 'mkam.sh', it will create an app with the name 'myApp'.
+#
+
+if [ "$1" != "" ]; then
+    appName=$1
+else
+    appName='myApp'
+fi
+
+meteor create appName
 
 # Next, let us get rid of things that are in the way.
 meteor remove blaze-html-templates
